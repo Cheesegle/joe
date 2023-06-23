274e92082c8b9c9ef8fcaac6a7c3aafcdba48232
Cheesegle <collin9ex@gmail.com>
2023-06-23 17:41:41 -0400

    client overhaul

--- public/script.js
+++ public/script.js
@@ -1,87 +1,79 @@
 var socket = io();
+var keyState = {};
+var mapData = [];
+var playerList = {};
+var playerListOld = {};
+var lastTick = performance.now();
+var zoom = 3000;
+var gameScale = (window.innerWidth + window.innerHeight) / zoom;
+var playerId = "";
+var fps = 0;
+
+var mouseXOffset = 0;
+var mouseYOffset = 0;
+var edgeOffset = 400; // Adjust this value to change the distance from the edge for triggering the translation
+
+var cameraX = 0;
+var cameraY = 0;
+var targetCameraX = 0;
+var targetCameraY = 0;
+var cameraLerpAmount = 0.1; // Adjust this value to change the smoothness of camera movement
 
 const getFPS = () =>
   new Promise(resolve =>
     requestAnimationFrame(t1 =>
       requestAnimationFrame(t2 => resolve(1000 / (t2 - t1)))
     )
-  )
+  );
 
-var keyState = {};
-window.addEventListener('keydown', function(e) {
+function handleKeyDown(e) {
   keyState[e.keyCode || e.which] = true;
-}, true);
-window.addEventListener('keyup', function(e) {
+}
+
+function handleKeyUp(e) {
   keyState[e.keyCode || e.which] = false;
-}, true);
+}
 
-window.addEventListener("contextmenu", (e) => {
-  e.preventDefault()
-});
+function handleContextMenu(e) {
+  e.preventDefault();
+}
 
-let map = [];
-
-fetch('/map').then(function(response) {
-  return response.json();
-}).then(function(data) {
-  map = data;
-}).catch(function(err) {
-  console.log('Fetch Error :-S', err);
-});
-
-let playerList = {};
-let playerListOld = {};
-let lastTick = performance.now();
-let zoom = 3000;
-let gameScale = (window.innerWidth + window.innerHeight) / zoom;
-let playerId = "";
-
-socket.on('id', (id) => {
-  playerId = id;
-});
-
-socket.on('tick', (playerTickList) => {
-  playerListOld = playerList;
-  playerList = playerTickList;
-  lastTick = performance.now()
-});
-
-socket.on('remove', (id) => {
-  delete map[id];
-});
-
-let fps = 0;
+function fetchMap() {
+  fetch('/map')
+    .then(response => response.json())
+    .then(data => {
+      mapData = data;
+    })
+    .catch(err => {
+      console.log('Fetch Error :-S', err);
+    });
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   getFPS().then(fps => {
-    let fpsRound = Math.round(fps)
-    if (fpsRound > 60) {
-      frameRate(fpsRound)
-    } else {
-      frameRate(60)
-    }
+    let fpsRound = Math.round(fps);
+    frameRate(fpsRound > 60 ? fpsRound : 60);
   });
 }
 
 function playerLerp(id) {
-  let player = playerList[id]
-  let playerOld = playerListOld[id]
-  let tickProgress = (performance.now() - lastTick) / (1000 / 64)
-  let lerpX = lerp(playerOld.x, player.x, tickProgress)
-  let lerpY = lerp(playerOld.y, player.y, tickProgress)
+  let player = playerList[id];
+  let playerOld = playerListOld[id];
+  let tickProgress = (performance.now() - lastTick) / (1000 / 64);
+  let lerpX = lerp(playerOld.x, player.x, tickProgress);
+  let lerpY = lerp(playerOld.y, player.y, tickProgress);
   return {
     x: lerpX,
     y: lerpY
-  }
+  };
 }
 
-setInterval(function() {
+function updateFPS() {
   fps = frameRate();
-}, 1000);
+}
 
 function draw() {
-
   const keyEvents = {
     87: 'w',
     65: 'a',
@@ -104,55 +96,60 @@ function draw() {
   text("FPS: " + fps.toFixed(2), 10, height - 10);
 
   if (playerListOld[playerId]) {
-    let lerpPos = playerLerp(playerId)
-    translate((-lerpPos.x * gameScale) + (windowWidth / 2), (-lerpPos.y * gameScale) + (windowHeight / 2));
+    let lerpPos = playerLerp(playerId);
+    targetCameraX = (-lerpPos.x * gameScale) + (windowWidth / 2) + mouseXOffset;
+    targetCameraY = (-lerpPos.y * gameScale) + (windowHeight / 2) + mouseYOffset;
+    cameraX = lerp(cameraX, targetCameraX, cameraLerpAmount);
+    cameraY = lerp(cameraY, targetCameraY, cameraLerpAmount);
+    translate(cameraX, cameraY);
   }
 
   scale(gameScale);
 
   for (const id in playerList) {
     if (playerListOld[id]) {
-      let lerpPos = playerLerp(id)
-      push()
-      // fill('blue')
-      // ellipse(playerList[id].x, playerList[id].y, 80, 80);
-      fill('green')
+      let lerpPos = playerLerp(id);
+      push();
+      fill('green');
       ellipse(lerpPos.x, lerpPos.y, 80, 80);
-      pop()
+      pop();
     }
   }
 
-  let highlight = {};
+  let highlight = null;
 
   if (playerList[playerId]) {
-    for (const id in map) {
-      if (map[id]) {
-        let tile = map[id]
+    for (const id in mapData) {
+      if (mapData[id]) {
+        let tile = mapData[id];
+        let tileMinX = (tile.minX - playerList[playerId].x) * gameScale + mouseXOffset;
+        let tileMaxX = (tile.maxX - playerList[playerId].x) * gameScale + mouseXOffset;
+        let tileMinY = (tile.minY - playerList[playerId].y) * gameScale + mouseYOffset;
+        let tileMaxY = (tile.maxY - playerList[playerId].y) * gameScale + mouseYOffset;
+
         if (
-          tile.minY > playerList[playerId].y - ((zoom / (windowWidth + windowHeight)) * (windowHeight / 2)) - 240 &&
-          tile.minY < playerList[playerId].y + ((zoom / (windowWidth + windowHeight)) * (windowHeight / 2)) + 240 &&
-          tile.minX > playerList[playerId].x - ((zoom / (windowWidth + windowHeight)) * (windowWidth / 2)) - 240 &&
-          tile.minX < playerList[playerId].x + ((zoom / (windowWidth + windowHeight)) * (windowWidth / 2)) + 240
+          tileMaxX > -windowWidth / 2 && tileMinX < windowWidth / 2 &&
+          tileMaxY > -windowHeight / 2 && tileMinY < windowHeight / 2
         ) {
           if (
-            mouseX - windowWidth / 2 > (tile.minX - playerList[playerId].x) * gameScale &&
-            mouseY - windowHeight / 2 > (tile.minY - playerList[playerId].y) * gameScale &&
-            mouseX - windowWidth / 2 < (tile.maxX - playerList[playerId].x) * gameScale &&
-            mouseY - windowHeight / 2 < (tile.maxY - playerList[playerId].y) * gameScale
+            mouseX - windowWidth / 2 > tileMinX &&
+            mouseY - windowHeight / 2 > tileMinY &&
+            mouseX - windowWidth / 2 < tileMaxX &&
+            mouseY - windowHeight / 2 < tileMaxY
           ) {
             if (mouseIsPressed === true) {
-              socket.emit('remove', tile, id)
+              socket.emit('remove', tile, id);
             } else {
               highlight = tile;
             }
           } else {
-            push()
+            push();
             stroke(156, 39, 176);
             fill(244, 122, 158);
             noFill();
             strokeWeight(4);
             rect(tile.minX, tile.minY, tile.maxX - tile.minX, tile.maxY - tile.minY);
-            pop()
+            pop();
           }
         }
       }
@@ -160,25 +157,72 @@ function draw() {
   }
 
   if (highlight) {
-    push()
+    push();
     stroke(255);
     fill(244, 122, 158);
     noFill();
     strokeWeight(8);
     rect(highlight.minX, highlight.minY, highlight.maxX - highlight.minX, highlight.maxY - highlight.minY);
-    pop()
+    pop();
   }
-
-
 }
 
 function mouseWheel(event) {
-  zoom += event.delta * 3;
+  const zoomDelta = event.delta * 3;
+  const prevZoom = zoom;
+  zoom += zoomDelta;
   zoom = constrain(zoom, 3000, 16000);
   gameScale = (window.innerWidth + window.innerHeight) / zoom;
+
+  const zoomFactor = zoom / prevZoom;
+  const dx = (windowWidth / 2 - cameraX) / gameScale;
+  const dy = (windowHeight / 2 - cameraY) / gameScale;
+  const newCameraX = windowWidth / 2 - dx * zoomFactor * gameScale;
+  const newCameraY = windowHeight / 2 - dy * zoomFactor * gameScale;
+
+  targetCameraX += cameraX - newCameraX;
+  targetCameraY += cameraY - newCameraY;
+  cameraX = targetCameraX;
+  cameraY = targetCameraY;
 }
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
   gameScale = (window.innerWidth + window.innerHeight) / zoom;
 }
+
+function mouseMoved() {
+  mouseXOffset = 0;
+  mouseYOffset = 0;
+
+  // Check if the mouse is near the edges of the canvas
+  if (mouseX < edgeOffset) {
+    mouseXOffset = edgeOffset - mouseX;
+  } else if (mouseX > width - edgeOffset) {
+    mouseXOffset = width - edgeOffset - mouseX;
+  }
+
+  if (mouseY < edgeOffset) {
+    mouseYOffset = edgeOffset - mouseY;
+  } else if (mouseY > height - edgeOffset) {
+    mouseYOffset = height - edgeOffset - mouseY;
+  }
+}
+
+// Event listeners
+window.addEventListener('keydown', handleKeyDown, true);
+window.addEventListener('keyup', handleKeyUp, true);
+window.addEventListener('contextmenu', handleContextMenu);
+socket.on('id', (id) => {
+  playerId = id;
+});
+socket.on('tick', (playerTickList) => {
+  playerListOld = playerList;
+  playerList = playerTickList;
+  lastTick = performance.now();
+});
+socket.on('remove', (id) => {
+  delete mapData[id];
+});
+setInterval(updateFPS, 1000);
+fetchMap();
